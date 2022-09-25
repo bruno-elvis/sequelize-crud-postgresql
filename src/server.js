5670d6bc6ffee79b6f0f3a5f255ef3a9bd9aa062
Bruno-Elvis <brunoelvis@outlook.com.br>
2022-09-25 20:40:12 -0300

    Config do CORS

--- src/server.js
+++ src/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 const port = process.env.PORT || 5555;
 
@@ -7,6 +8,7 @@ require('./database');
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
 
-app.listen(port, port => console.log(`Server rodando na porta ${port}`));
+app.listen(port, () => console.log(`Server rodando na porta ${port}`));
